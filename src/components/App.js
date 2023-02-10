f3f5c36e65614755a38fd1178c8dd9be0be61a09
MarianaKely <marianakely.r@gmail.com>
2023-02-10 19:59:32 -0300

    mobile started = MObileheaderbar.js implemented

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import Headerbar from "./Headerbar";
+import Mobileheaderbar from "./Mobileheaderbar";
 import Pageorganization from "./Pageorganization";
+
 
 export default function App({ pageconstruction }) {
     return (
       <div className="container">
         <Headerbar />
+        <Mobileheaderbar />
         <Pageorganization pageconstruction={pageconstruction} />
       </div>
     );
---
+++ src/components/Mobileheaderbar.js
@@ -0,0 +1,14 @@
+export default function Mobileheaderbar () {
+    
+    return (
+
+      <div className="bigboxmobileheader">
+       <div className="mobileheaderbar">
+        <ion-icon name="logo-instagram"></ion-icon>
+        <img src="./assets/logo.png" />
+        <ion-icon name="paper-plane-outline"></ion-icon>
+       </div>
+      </div>
+
+    );
+}
