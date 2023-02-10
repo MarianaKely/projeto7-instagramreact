6a0a34d09e2bb65551b7b9b0e431051b88d63536
MarianaKely <marianakely.r@gmail.com>
2023-02-09 22:43:15 -0300

    Sidebar.js page update

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,12 +1,7 @@
-import Userbar from "./Userbar";
-import Suggestedprofiles from "./Suggestedprofiles";
-
 export default function Sidebar({ pageconstruction }) {
-  const { userprofile, sugestionbox } = pageconstruction;
+  
   return (
     <div className="sidebar">
-      <Userbar userprofile={userprofile} />
-      <Suggestedprofiles sugestionbox={sugestionbox} />
     </div>
   );
 }
